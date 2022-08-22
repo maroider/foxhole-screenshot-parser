64c194e82ef5d3cd8fcc17627b7ba490dc6756a6
Markus Røyset <maroider@protonmail.com>
2022-08-22 04:38:02 +0200

    Flesh out the xtask some more

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,6 +1,9 @@
-use std::path::{
-    Path,
-    PathBuf,
+use std::{
+    iter,
+    path::{
+        Path,
+        PathBuf,
+    },
 };
 
 use duct::cmd;
@@ -10,14 +13,17 @@ fn main() {
     let mut args = Arguments::from_env();
     if let Some(command) = args.subcommand().unwrap() {
         match &*command {
-            "fetch" => {
-                fetch();
+            "run" => {
+                run(&mut args);
+            }
+            "dist" => {
+                build(true);
             }
             "build-ueviewer" => {
                 build_ueviewer();
             }
-            "dist" => {
-                dist();
+            "fetch" => {
+                fetch();
             }
             _ => {
                 println!("Unknown command '{command}'");
@@ -28,11 +34,64 @@ fn main() {
     }
 }
 
+fn run(args: &mut Arguments) {
+    let release = args.contains("--release");
+    build(release);
+}
+
+fn build(release: bool) {
+    build_ueviewer();
+
+    println!("Building foxhole-stockpile-screenparser");
+    cmd(
+        "cargo",
+        iter::once("build").chain(release.then_some("--release")),
+    )
+    .run()
+    .unwrap();
+
+    let dist_dir = &build_dir().join(if release { "dist" } else { "debug" });
+    std::fs::create_dir_all(dist_dir).unwrap();
+    let exe_name = &get_exe_name("foxhole-stockpile-screenparser");
+    std::fs::copy(
+        Path::new(env!("CARGO_MANIFEST_DIR"))
+            .parent()
+            .unwrap()
+            .join("target")
+            .join("release")
+            .join(exe_name),
+        dist_dir.join(exe_name),
+    )
+    .unwrap();
+    let exe_name = &get_exe_name("umodel");
+    std::fs::copy(ueviewer_build_dir().join(exe_name), dist_dir.join(exe_name)).unwrap();
+}
+
+fn build_ueviewer() {
+    fetch();
+
+    println!("Building UEViewer");
+    let dir = &ueviewer_build_dir();
+    cmd!(dir.join("build.sh")).dir(dir).run().unwrap();
+}
+
 fn fetch() {
     let ueviewer_ref = "c444911a6ad65bff5266f273dd5bdf7dd6fb506e";
-    println!("Fetching UEViewer at {ueviewer_ref}");
     let dir = &ueviewer_build_dir();
     std::fs::create_dir_all(dir).unwrap();
+
+    let output = cmd!("git", "rev-parse", "HEAD")
+        .dir(dir)
+        .unchecked()
+        .stdout_capture()
+        .stderr_capture()
+        .run()
+        .unwrap();
+    if output.status.success() && String::from_utf8(output.stdout).unwrap().trim() == ueviewer_ref {
+        return;
+    }
+
+    println!("Fetching UEViewer at {ueviewer_ref}");
     cmd!("git", "init").dir(dir).run().unwrap();
     let remotes = cmd!("git", "remote").dir(dir).read().unwrap();
     if remotes.is_empty() {
@@ -61,18 +120,22 @@ fn fetch() {
         .unwrap();
 }
 
-fn build_ueviewer() {
-    println!("build");
-}
-
-fn dist() {
-    println!("dist");
-}
-
 fn ueviewer_build_dir() -> PathBuf {
+    build_dir().join("UEViewer")
+}
+
+fn build_dir() -> PathBuf {
     Path::new(env!("CARGO_MANIFEST_DIR"))
         .parent()
         .unwrap()
         .join("build")
-        .join("UEViewer")
+}
+
+fn get_exe_name(name: &str) -> String {
+    let suffix = if cfg!(target_os = "windows") {
+        ".exe"
+    } else {
+        ""
+    };
+    format!("{name}{suffix}")
 }
