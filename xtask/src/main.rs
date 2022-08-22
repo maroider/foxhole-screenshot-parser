0d2710256bbb77e3e9eedeec13b946f5eeadda2f
Markus Røyset <maroider@protonmail.com>
2022-08-22 03:29:39 +0200

    Sparsely checkout UEViewer

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,9 @@
+use std::path::{
+    Path,
+    PathBuf,
+};
+
+use duct::cmd;
 use pico_args::Arguments;
 
 fn main() {
@@ -23,7 +29,36 @@ fn main() {
 }
 
 fn fetch() {
-    println!("fetch");
+    let ueviewer_ref = "c444911a6ad65bff5266f273dd5bdf7dd6fb506e";
+    println!("Fetching UEViewer at {ueviewer_ref}");
+    let dir = &ueviewer_build_dir();
+    std::fs::create_dir_all(dir).unwrap();
+    cmd!("git", "init").dir(dir).run().unwrap();
+    let remotes = cmd!("git", "remote").dir(dir).read().unwrap();
+    if remotes.is_empty() {
+        cmd!(
+            "git",
+            "remote",
+            "add",
+            "origin",
+            "https://github.com/gildor2/UEViewer",
+        )
+        .dir(dir)
+        .run()
+        .unwrap();
+    }
+    cmd!("git", "fetch", "origin", ueviewer_ref, "--depth", "1")
+        .dir(dir)
+        .run()
+        .unwrap();
+    cmd!("git", "config", "advice.detachedHead", "false")
+        .dir(dir)
+        .run()
+        .unwrap();
+    cmd!("git", "checkout", ueviewer_ref)
+        .dir(dir)
+        .run()
+        .unwrap();
 }
 
 fn build_ueviewer() {
@@ -32,4 +67,12 @@ fn build_ueviewer() {
 
 fn dist() {
     println!("dist");
+}
+
+fn ueviewer_build_dir() -> PathBuf {
+    Path::new(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .unwrap()
+        .join("build")
+        .join("UEViewer")
 }
